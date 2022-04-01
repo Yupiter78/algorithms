8342122bd1e0a48f65584693dcdcfd9768b0e839
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-04-01 23:57:43 +0300

    feat: add quickSort

--- quickSort.js
+++ quickSort.js
@@ -29,3 +29,33 @@ function quickSort(arr) {
 const testArr = [2, 5, 56, 1, 4, 7, 8, 3, 12, 34, 89, 12, 19];
 
 console.log("quickSort(testArr):", quickSort(testArr));
+
+function partition(arr, start, end){
+    const pivotValue = arr[end];
+    let pivotIndex = start;
+    for (let i = start; i < end; i++) {
+        if (arr[i] < pivotValue) {
+            [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+            pivotIndex++;
+        }
+    }
+
+    [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]]
+    return pivotIndex;
+};
+
+function quickSortRecursive(arr, start, end) {
+    if (start >= end) {
+        return;
+    }
+
+    let index = partition(arr, start, end);
+
+    quickSort(arr, start, index - 1);
+    quickSort(arr, index + 1, end);
+}
+
+const arrayForTest = [2, 5, 56, 1, 4, 7, 8, 3, 12, 34, 89, 12, 19];
+
+quickSortRecursive(arrayForTest, 0, arrayForTest.length - 1);
+console.log("arrayForTest:", arrayForTest);
