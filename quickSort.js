075a06bf6dcfa31479136da65264ce75628c13e6
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-04-02 22:45:44 +0300

    feat: add quickSort iterative

--- quickSort.js
+++ quickSort.js
@@ -55,5 +55,41 @@ function quickSortRecursive(arr, start, end) {
 const arrayForTest = [2, 5, 56, 1, 4, 7, 8, 3, 12, 34, 89, 12, 19];
 
 quickSortRecursive(arrayForTest, 0, arrayForTest.length - 1);
-console.log("arrayForTest:", arrayForTest);
+console.log("arrayForTest_recursive:", arrayForTest);
 
+function quickSortIterative(arr) {
+    // Создание массива, который мы будем использовать в качестве стека, с помощью функций push() и pop()
+    const stack = [];
+
+    // Добавление всего исходного массива в виде «несортированного подмассива»
+    stack.push(0);
+    stack.push(arr.length - 1);
+
+    // Цикл повторяется до тех пор, пока у нас есть несортированные подмассивы.
+    while(stack[stack.length - 1] >= 0){
+
+        // Извлечение верхнего несортированного подмассива
+         const end = stack.pop();
+        const start = stack.pop();
+
+        const pivotIndex = partition(arr, start, end);
+
+        // Если «слева» от опорной точки есть несортированные элементы,
+        // мы добавляем этот подмассив в stack, чтобы отсортировать его позже.
+        if (pivotIndex - 1 > start){
+            stack.push(start);
+            stack.push(pivotIndex - 1);
+        }
+
+        // Если «справа» от опорной точки есть несортированные элементы,
+        // мы добавляем этот подмассив в stack, чтобы отсортировать его позже.
+        if (pivotIndex + 1 < end){
+            stack.push(pivotIndex + 1);
+            stack.push(end);
+        }
+    }
+}
+
+
+quickSortIterative(arrayForTest);
+console.log("arrayForTest_iterative:", arrayForTest);
