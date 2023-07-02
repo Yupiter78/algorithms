611ec5432ce152ce09c2871a7ad042701f04f394
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-07-02 23:58:54 +0300

    feat: add task greedy_algorithm in algorithms

--- greedy_algorithm/greedy_algorithm.js
+++ greedy_algorithm/greedy_algorithm.js
@@ -16,4 +16,19 @@ function change(money, coins) {
     }, {});
 }
 
+const obj = change(167, [25, 10, 5, 1]);
+console.log(JSON.stringify(obj));
+
 console.log(change(167, [25, 10, 5, 1]));
+
+function change_2(money, coins) {
+    const changeStr = coins.reduce((prev, cur) => {
+        const quantity = Math.floor( money / cur);
+        money -= cur * quantity;
+        return prev + `"${cur}":${quantity},`;
+    }, '');
+
+    return JSON.parse(`{${changeStr.slice(0, -1)}}`);
+}
+
+console.log(change_2(167, [25, 10, 5, 1]));
